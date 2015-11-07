3f747ea41b2bbaf5ae385217e892abf19af8c04b
Marius Sebastian Trif <coolsebz@gmail.com>
2015-11-07 15:40:11 +0200

    Adding content to the default asset file for the API

---
+++ api/config/assets/default.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+
+  // note(seb): this is just a server setup, so that's why there are no client settings
+  server: {
+    gulpConfig: 'gulpfile.js',
+    allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
+    models: 'modules/*/server/models/**/*.js',
+    collections: 'modules/*/server/collections/**/*.js',
+    routes: ['modules/!(core)/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
+    sockets: 'modules/*/server/sockets/**/*.js',
+    config: 'modules/*/server/config/*.js',
+    policies: 'modules/*/server/policies/*.js',
+    views: 'modules/*/server/views/*.html'
+  },
+  client: { }
+};
